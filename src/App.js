317afe96ca3c90d89bb40164d6e34cb14ddae16e
Przemysław Wnęk <wnek.przemek@gmail.com>
2021-11-08 21:59:07 +0100

    add data from pizzeria, yarn start:api bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-
+import React from 'react';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactzcxcc
         </a>
       </header>
     </div>
